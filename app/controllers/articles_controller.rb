e2cb408ed9e269ae40094c71f475911d1fc7d247
Alban Fournier <alban.fournier@hotmail.fr>
2018-05-28 15:50:26 +0200

    Update current_user to be article creator

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,11 @@
 class ArticlesController < ApplicationController
 before_action :set_article, only: [:edit, :update, :show, :destroy]
+before_action :require_user, except: [:index, :show]
+before_action :require_same_user, only: [:edit, :update, :destroy]
 
 	def index
-	@articles = Article.all
-end
+		@articles = Article.paginate(page: params[:page], per_page: 5)
+	end
 
 	def new
 		@article = Article.new
@@ -15,7 +17,7 @@ end
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:success] = "Article was successfully created"
 			redirect_to article_path(@article)
@@ -32,7 +34,6 @@ end
 		else render'edit'
 		end
 	end
-
 
 	def show
 		
@@ -53,4 +54,16 @@ end
 			params.require(:article).permit(:title, :description)
 		end
 
+	def require_same_user
+	if current_user != @article.user 
+		flash[:danger] = "You can only edit or delete your own articles"
+		redirect_to root_path
+	end
 end
+
+
+end
+
+
+
+
